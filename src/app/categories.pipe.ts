923124b35f80d8c84f6af2e6d4cd6cd23a4a12f3
MaisAbuJarur <maisabujarur@gmail.com>
2017-03-18 15:00:35 +0200

    home page

--- src/app/categories.pipe.ts
+++ src/app/categories.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { HomeComponent } from './home/home.component'
-
 @Pipe({
   name: 'categories'
 })
 export class CategoriesPipe implements PipeTransform {
 
-    transform(categories: Array<any>, selectedCat: string): Array<any> {
-        return categories.filter(ad => ad.ad_cat === selectedCat);
+     transform(value: any, [catg]): any {
+        return value.filter((item) => item.ad_cat === catg);
     }
 }
+
+
+
+
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,32 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { GetaddService } from '../getadd.service';
+import {GetaddService } from '../getadd.service';
+import {CategoriesPipe} from '../categories.pipe';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
+  
+  
 })
 export class HomeComponent implements OnInit {
-  categories: any;
+  //pipes///
+  
+  catg:any = ["Careers","Cars","Furniture","Electronics","Other"];
+  ///////////
 	alladds:any;
-  selectedCat :string;
   constructor(private Get:GetaddService) {
   this.Get.getalladv().subscribe(ok=>{
     this.alladds=ok
     console.log(this.alladds)
   });
-
-  this.categories = ["careers", "cars", "furniture" ]    
-
    }
 
   ngOnInit() {
-    
-
+     
 
   }
-
-  
-  	
-  
-
 }
